a7fe06eff723d92afe372ec1d5525c6168e66ed5
zzydsb666 <13326956297@163.com>
2026-07-07 09:42:44 +0800

    first save

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+typedef struct Student{
+    int id;
+    char name[20];
+}Stu;
+
+int main(){
+    Stu s1 = {202601, "学生张三"};
+    printf("学号：%d，姓名：%s",s1.id,s1.name);
+    return 0;
+}
